5512d278bedd2a5cad8b38d7dd575b7c86a11d03
Jonas <jroothans@gmail.com>
2025-07-10 12:01:12 +0200

    brainlabextractor based on actors

--- add-ons/BrainlabExtractor/BrainlabExtractor_AssignMaster.m
+++ add-ons/BrainlabExtractor/BrainlabExtractor_AssignMaster.m
@@ -2,11 +2,20 @@ function [outputArg1,outputArg2] = BrainlabExtractor_AssignMaster(menu,eventdata
 %BRAINLABEXTRACTOT_ASSIGNMASTER Summary of this function goes here
 %   Detailed explanation goes here
 
-[filename,foldername] = uigetfile('*.nii','Locate the native template');
-if filename==0
-    return
+[actorlist,namelist,indexlist] =  ArenaScene.getActorsOfClass(scene,'Slicei');
+indx = listdlg("PromptString",'Select the image','ListString',[{'...browse'},namelist],'ListSize',[300,160]);
+if indx==1
+    [filename,foldername] = uigetfile('*.nii','Locate the native template');
+    if filename==0
+        return
+    end
+else
+    thisActor = actorlist(indx-1);
+    foldername = tempdir;
+    filename = thisActor.Tag;
+    thisActor.Data.parent.savenii(fullfile(foldername,filename))
 end
-    
+
 %update the menu text
 splt = strsplit(foldername(1:end-1),'/');
 menuname = [splt{end},filesep,filename];
--- add-ons/BrainlabExtractor/BrainlabExtractor_AssignOther.m
+++ add-ons/BrainlabExtractor/BrainlabExtractor_AssignOther.m
@@ -3,8 +3,11 @@ function [outputArg1,outputArg2] = BrainlabExtractor_AssignBI(menu,eventdata,sce
 %   Detailed explanation goes here
 
 %BI stands for Burned-In
+[actorlist,namelist,indexlist] =  ArenaScene.getActorsOfClass(scene,'Slicei');
+indx = listdlg("PromptString",'Select other image(s)','ListString',[{'...browse'},namelist],'ListSize',[300,160],'SelectionMode','multiple');
 
-[Otherfilename,Otherfoldername] = uigetfile('*.nii','Get other images','MultiSelect','on');
+if indx==1
+    [Otherfilename,Otherfoldername] = uigetfile('*.nii','Get other images','MultiSelect','on');
     if ~iscell(Otherfilename)
         if Otherfilename==0
             return
@@ -14,14 +17,20 @@ function [outputArg1,outputArg2] = BrainlabExtractor_AssignBI(menu,eventdata,sce
     if not(iscell(Otherfilename))
         Otherfilename = {Otherfilename};
     end
+else
+    Otherfilename = {};
+    Otherfoldername = tempdir;
+    for i = indx
+        thisActor = actorlist(i-1);
+        filename = thisActor.Tag;
+        thisActor.Data.parent.savenii(fullfile(Otherfoldername,filename))
+        Otherfilename{end+1} = filename;
+    end
 menu.Text = ['Other: ',num2str(numel(Otherfilename))];
 
 
 %save the filename
 menu.Parent.UserData.Otherfilename = Otherfilename;
 menu.Parent.UserData.Otherfoldername = Otherfoldername;
-    
-
-
 end
 
