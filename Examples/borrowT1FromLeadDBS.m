e6ef52e8174c798705c2051f4568b936f2c9b52d
Jonas <jroothans@gmail.com>
2026-05-06 14:12:17 +0200

    example fix

--- Examples/borrowT1FromLeadDBS.m
+++ Examples/borrowT1FromLeadDBS.m
@@ -2,6 +2,15 @@ function borrowT1FromLeadDBS()
     disp('trying to copy t1.nii from lead-dbs')
     load(fullfile(ArenaManager.getrootdir(),'config.mat'))
     from = fullfile(config.leadDBS,'templates','space','MNI_ICBM_2009b_NLIN_ASYM','t1.nii');
+    if ~exist(from)
+        from = fullfile(config.leadDBS,'templates','space','MNI152NLin2009bAsym','t1.nii');
+        if ~exist(from)
+            print('Unable to find t1.nii')
+            return
+        end
+    end
     to = fullfile(fileparts(mfilename('fullpath')),'t1.nii');
+
     copyfile(from,to)
+    disp('copy succesful')
 end
